ab4c579d431dfc130ea4dc6a83c8faafb8994e7d
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-15 15:19:03 -0500

    Get the images of cats with their names from the AJAX call

--- cats.js
+++ cats.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   var summonCats = document.querySelector('.summon-cats');
+  var catBoxes = document.getElementsByClassName('cat-box');
   console.log(summonCats);
 
   summonCats.addEventListener('click', function(e) {
     console.log("Summoning Cats");
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+    }).done(function(data) {
+        var cats = data.cats
+        var catPics = cats.map(function(cat) {
+          var image = document.createElement('img');
+          image.src = cat.photo;
+          image.alt = "Photo of " + cat.name;
+        });
+        
   });
 
 });
